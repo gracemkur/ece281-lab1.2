d667074f28b3ca0e7fa160b973a8993ecfc97a2f
 <github noreply email@users.noreply.github.com>
2025-02-11 21:35:47 -0700

    aahhhhh

--- src/thirtyOneDayMonth_tb.vhd
+++ src/thirtyOneDayMonth_tb.vhd
@@ -93,9 +93,9 @@ begin
 		w_sw <= x"0"; wait for 10 ns;
             assert w_Y = '0' report "error on x0" severity failure;
         w_sw <= x"1"; wait for 10 ns;
-            assert w_Y = '0' report "error on Jan" severity failure;
+            assert w_Y = '1' report "error on Jan" severity failure;
         w_sw <= x"2"; wait for 10 ns;
-            assert w_Y = '1' report "error on Feb" severity failure;
+            assert w_Y = '0' report "error on Feb" severity failure;
         w_sw <= x"3"; wait for 10 ns;
             assert w_Y = '1' report "error on Mar" severity failure;
         w_sw <= x"4"; wait for 10 ns;
@@ -119,7 +119,7 @@ begin
         w_sw <= x"D"; wait for 10 ns;
             assert w_Y = '0' report "error on xD" severity failure;
         w_sw <= x"E"; wait for 10 ns;
-            assert w_Y = '0' report "error on xE" severity failure;
+            assert w_Y = '1' report "error on xE" severity failure;
         w_sw <= x"F"; wait for 10 ns;
             assert w_Y = '0' report "error on xF" severity failure;
 		wait; -- wait forever
